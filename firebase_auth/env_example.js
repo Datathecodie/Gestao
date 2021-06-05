b9a7c3df5a0a989784130a35b9e2a3b64bc9101b
Dattaprasad Ekavade <ekavade@gmail.com>
2021-06-05 17:33:57 +0530

    Adding Env_example

---
+++ firebase_auth/env_example.js
@@ -0,0 +1,14 @@
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "example",
+    authDomain: "example",
+    projectId: "example",
+    storageBucket: "example",
+    messagingSenderId: "example",
+    appId: "example",
+    measurementId: "example"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
