dd6b27bc9ea68bff4923f98b287fe20c63f92163
Swetadigitcure <sweta.digitcure@gmail.com>
2021-06-15 11:13:48 +0530

    Create auth.js

---
+++ firebase_auth/auth.js
@@ -0,0 +1,12 @@
+ // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyD5Y8XOvUMliOEGDt9b3-vFYpeupyzMtlw",
+    authDomain: "gestao-firebase-315714.firebaseapp.com",
+    projectId: "gestao-firebase-315714",
+    storageBucket: "gestao-firebase-315714.appspot.com",
+    messagingSenderId: "372323939867",
+    appId: "1:372323939867:web:3c3041f0b2732c57c36abe",
+    measurementId: "G-M8WS8B0XNX"
+  };
+  // Initialize Firebase
